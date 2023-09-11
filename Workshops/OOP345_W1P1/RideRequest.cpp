862d19892eba510a10604f58b90a0db2ab7d5075
fmoayeri2 <fmoayeri2@myseneca.ca>
2023-09-10 22:13:06 -0400

    W1P1: Added .gitignore and student info at top of mudles

--- Workshops/OOP345_W1P1/RideRequest.cpp
+++ Workshops/OOP345_W1P1/RideRequest.cpp
@@ -1,9 +1,20 @@
+///////////////////////////////////////////////////////
+//                  WorkShop 1 - Part 1
+// Name: Farbod Moayeri
+// Id: 134395227
+// Email: fmoayeri2@myseneca.ca
+// Section: NFF
+// Date: 2023-09-10
+///////////////////////////////////////////////////////
+// I have done all the coding by myself and only copied
+// the code that my professor provided to complete my 
+// workshops and assignments.
+///////////////////////////////////////////////////////
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstring>
 #include <iomanip>
-// #include <string>
 #include "RideRequest.h"
-
 
 namespace sdds {
 
--- Workshops/OOP345_W1P1/RideRequest.h
+++ Workshops/OOP345_W1P1/RideRequest.h
@@ -1,3 +1,16 @@
+///////////////////////////////////////////////////////
+//                  WorkShop 1 - Part 1
+// Name: Farbod Moayeri
+// Id: 134395227
+// Email: fmoayeri2@myseneca.ca
+// Section: NFF
+// Date: 2023-09-10
+///////////////////////////////////////////////////////
+// I have done all the coding by myself and only copied
+// the code that my professor provided to complete my 
+// workshops and assignments.
+///////////////////////////////////////////////////////
+
 #ifndef SDDS_RIDEREQUEST_H
 #define SDDS_RIDEREQUEST_H
 
