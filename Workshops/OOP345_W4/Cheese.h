e0b7bdb211c58a92b8cb73ac30e7256a746f86d4
fmoayeri2 <fmoayeri2@myseneca.ca>
2023-10-05 14:14:16 -0400

    W4: Part 1 complete

--- Workshops/OOP345_W4/Cheese.h
+++ Workshops/OOP345_W4/Cheese.h
@@ -1,3 +1,16 @@
+///////////////////////////////////////////////////////
+//                  WorkShop 4 - Part 2
+// Name: Farbod Moayeri
+// Id: 134395227
+// Email: fmoayeri2@myseneca.ca
+// Section: NFF
+// Date: 2023-09-23
+///////////////////////////////////////////////////////
+// I have done all the coding by myself and only copied
+// the code that my professor provided to complete my 
+// workshops and assignments.
+///////////////////////////////////////////////////////
+
 #ifndef SDDS_CHEESE_H
 #define SDDS_CHEESE_H
 
@@ -9,16 +22,17 @@ namespace sdds {
 		int m_weight{ 0 };
 		double m_price{ 0.0 };
 		std::string m_features{};
+		virtual std::string trim(const std::string& inc) const;
 	public:
 		Cheese() = default;
 		Cheese(const std::string& str);
 		virtual Cheese slice(size_t weight);
 		// rule of five
-		virtual ~Cheese();
+		virtual ~Cheese() = default;
 		Cheese(const Cheese& inc);
-		Cheese& operator=(const Cheese& inc);
-		Cheese(Cheese&& inc);
-		Cheese& operator=(Cheese&& inc);
+		virtual Cheese& operator=(const Cheese& inc);
+		Cheese(Cheese&& inc) noexcept;
+		virtual Cheese& operator=(Cheese&& inc) noexcept;
 		// rule of five
 
 		// getters
@@ -26,6 +40,7 @@ namespace sdds {
 		int getWeight() const;
 		double getPrice() const;
 		std::string getFeatures() const;
+		// getters
 	};
 
 	std::ostream& operator<<(std::ostream& ostr, const Cheese& inc);
