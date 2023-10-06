ae4e1a93ebae91de5364b604a89140a417f272b7
Farbod Moayeri <Farbod Moayeri@LAPTOP-54JRQ3A1>
2023-10-06 12:46:35 -0400

    W4: Part 2 Progress

--- Workshops/OOP345_W4/CheeseShop.cpp
+++ Workshops/OOP345_W4/CheeseShop.cpp
@@ -20,18 +20,61 @@ namespace sdds {
 				local[i] = this->m_products[i];
 			}
 			
+			
+			*local[m_numProducts] = inc;
 			m_numProducts++;
 
-			*local[m_numProducts] = inc;
-
+			/*for (i = 0; i < m_numProducts; i++)
+			{
+				delete m_products[i];
+			}*/
+			
 			delete[] m_products;
 			m_products = local;
 		}
+
+		return *this;
 	}
 
 	CheeseShop::~CheeseShop()
 	{
 		size_t i{};
+
+		/*for (i = 0; i < m_numProducts; i++)
+		{
+			delete[] m_products[i];
+		}*/
+
+		delete[] m_products;
+	}
+
+	CheeseShop::CheeseShop(const CheeseShop& inc)
+	{
+
+	}
+
+	CheeseShop& CheeseShop::operator=(const CheeseShop& inc)
+	{
+		size_t i{};
+
+		if (this != &inc && inc.m_numProducts > 0)
+		{
+			for (i = 0; i < m_numProducts; i++)
+			{
+				delete[]
+			}
+		}
+
+		return *this;
+	}
+
+	CheeseShop::CheeseShop(CheeseShop&& inc) noexcept
+	{
+	}
+
+	CheeseShop& CheeseShop::operator=(CheeseShop&& inc) noexcept
+	{
+		// TODO: insert return statement here
 	}
 
 	std::ostream& operator<<(std::ostream& ostr, const CheeseShop& inc)
