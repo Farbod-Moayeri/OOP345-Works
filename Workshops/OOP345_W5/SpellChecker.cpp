86724cd7dea5af0eedd6eef0c32a32ff9f13cafb
Farbod Moayeri <fmoayeri2@myseneca.ca>
2023-10-13 22:25:54 -0400

    W5: Update

--- Workshops/OOP345_W5/SpellChecker.cpp
+++ Workshops/OOP345_W5/SpellChecker.cpp
@@ -5,6 +5,8 @@ namespace sdds {
 	SpellChecker::SpellChecker(const char* filename)
 	{
 		std::string contents{};
+		size_t numLines{};
+		size_t i{};
 
 		if (filename != nullptr)
 		{
@@ -12,9 +14,30 @@ namespace sdds {
 
 			if (file.is_open())
 			{
-				file.get(contents,)
+				while (file.good())
+				{
+					std::getline(file, contents, '\n');
+					numLines++;
+				}
+
+				file.clear();
+				file.seekg(0, std::ios::beg);
+
+				file >> contents;
+
+				for (i = 0; i < numLines; i++)
+				{
+					m_badWords[i] = contents.substr(0, contents.find(' '));
+					contents.erase(0, contents.find_last_of(' ') + 1);
+
+					m_goodWords[i] = contents.substr(0, contents.find(' '));
+					contents.erase(0, contents.find('\n') + 1);
+				}
+				
+				m_wordsStored = i;
+
+				file.close();
 			}
-			//while(m_badWords[5] == "" || )
 		}
 		else
 		{
@@ -23,7 +46,18 @@ namespace sdds {
 	}
 	void SpellChecker::operator()(std::string& text)
 	{
+		size_t i{};
+		size_t start{};
 
+		if (text.length() > 0)
+		{
+			for (i = 0; i < m_wordsStored; i++)
+			{
+				while(start = text.find())
+				text.replace(start,text.find(m_badWords[i]), m_goodWords[i]);
+
+			}
+		}
 	}
 	void SpellChecker::showStatistics(std::ostream& out) const
 	{
--- Workshops/OOP345_W5/SpellChecker.h
+++ Workshops/OOP345_W5/SpellChecker.h
@@ -2,11 +2,13 @@
 #define SDDS_SPELLCHECKER_H
 
 #include <string>
+
 namespace sdds {
 	class SpellChecker {
 		std::string m_badWords[6]{};
 		std::string m_goodWords[6]{};
-		int m_wordReplaced[6];
+		size_t m_wordReplaced[6]{};
+		size_t m_wordsStored{};
 	public:
 		SpellChecker(const char* filename);
 		void operator()(std::string& text);
